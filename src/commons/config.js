6541e56c76afacbd13ca83696704eaaa87f0663f
Irfan Maulana <irfanmaulana695@gmail.com>
2021-03-30 23:58:11 +0700

    add favicon & logo

--- src/commons/config.js
+++ src/commons/config.js
@@ -1,8 +1,8 @@
 // Development
-export const API_URL = 'http://localhost:8000/api/';
+// export const API_URL = 'http://localhost:8000/api/';
 
 // Production
-// export const API_URL = 'https://api.healthscore.smi-datas.com/api/';
+export const API_URL = 'https://api.healthscore.smi-datas.com/api/';
 
 // Python Production
 export const API_DS_URL = 'https://healthscore-smi.azurewebsites.net';
