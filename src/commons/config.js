ce06793e3ac91d546393627d5918cbad4ed64a3e
Irfan Maulana <irfanmaulana695@gmail.com>
2021-04-08 12:17:41 +0700

    Update dummy data

--- src/commons/config.js
+++ src/commons/config.js
@@ -1,8 +1,8 @@
 // Development
-export const API_URL = 'http://localhost:8000/api/';
+// export const API_URL = 'http://localhost:8000/api/';
 
 // Production
-// export const API_URL = 'https://api.healthscore.smi-datas.com/api/';
+export const API_URL = 'https://api.healthscore.smi-datas.com/api/';
 // export const API_URL = 'https://api.healthscore.semutmerah-analytics.com/api/';
 
 // Python Production
--- src/views/MasterUser.jsx
+++ src/views/MasterUser.jsx
@@ -9,13 +9,27 @@ import { UserService } from './../commons/api.service';
 
 import store from '../store';
 import { startLoading, stopLoading } from './../actions';
+import _ from 'lodash';
+
+const dummyUser = [
+    { id: '00081827384', name: 'John Doe', healthScore: 90, address: 'Jalan Menteng Kecil no.13', phone: '08288282134', illness: 'Cancer' },
+    { id: '00081827385', name: 'Jane Doe', healthScore: 70, address: 'Jalan Bangka no.21', phone: '08288188123', illness: 'Lupus, Cancer, Stroke' },
+    { id: '00081827386', name: 'Agus Septiadi', healthScore: 80, address: 'Jalan Senopati no.28', phone: '081808192873', illness: 'Stroke, Cancer' },
+    { id: '00081827387', name: 'Bambang', healthScore: 80, address: 'Jalan Kebon Pala no.16', phone: '08887178234', illness: 'Stroke, Lupus' },
+    { id: '00081827388', name: 'Maulana Cakra', healthScore: 90, address: 'Jalan Margonda Raya no.41', phone: '08181938425', illness: 'Heart Disease' },
+    { id: '00081827389', name: 'Irfan Maulana', healthScore: 90, address: 'Jalan Dukuh Pinggir no.15', phone: '08189184731', illness: 'Protozoal Disease, Unspecified Mental Disorder' },
+    { id: '00081827390', name: 'Khoiry Alfisyahr', healthScore: 90, address: 'Jalan Petojo Enclek no.11', phone: '08191871345', illness: 'Cancer, ' },
+    { id: '00081827391', name: 'Ilham Darmawan', healthScore: 96, address: 'Jalan Petamburan 2 no.19', phone: '08888719873', illness: 'Protozoal Disease' },
+    { id: '00081827392', name: 'Marpiero Romandung', healthScore: 94, address: 'Jalan Petamburan 3 no.22', phone: '08181891832', illness: 'Mood [affective] Disorders' }
+]
 
 export default class MasterUser extends Component {
     constructor(props) {
         super(props);
         this.state = {
             users: [],
-            isModalShow: false
+            isModalShow: false,
+            selectedUser: {}
         }
     }
 
@@ -30,11 +44,11 @@ export default class MasterUser extends Component {
         this.fetchUserList();
     }
 
-    handleShowModal = () => { this.setState({ isModalShow: true }) }
-    handleCloseModal = () => { this.setState({ isModalShow: false }) }
+    handleShowModal = (id) => { this.setState({ isModalShow: true, selectedUser: _.find(dummyUser, ['id', id]) }) }
+    handleCloseModal = () => { this.setState({ isModalShow: false, selectedUser: {} }) }
 
     render () {
-        const { users } = this.state;
+        const { selectedUser } = this.state;
 
         return (
             <div>
@@ -60,97 +74,17 @@ export default class MasterUser extends Component {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {users.map((values, key) => 
+                                        {dummyUser.map((values, key) => 
                                             <tr key={key}>
                                                 <td>{values.id}</td>
                                                 <td>{values.name}</td>
+                                                <td>{values.healthScore}</td>
                                                 <td>{values.address}</td>
                                                 <td>{values.phone}</td>
                                                 <td>{values.illness}</td>
-                                                <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
+                                                <td><button className="btn btn-primary btn-sm" onClick={() => this.handleShowModal(values.id)}>Detail</button></td>
                                             </tr>
                                         )}
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>000817172634</td>
-                                            <td>John Doe</td>
-                                            <td>83</td>
-                                            <td>Jalan Menteng Sukabumi no.13</td>
-                                            <td>08181819281</td>
-                                            <td>Cancer, Stroke</td>
-                                            <td><button className="btn btn-primary btn-sm" onClick={this.handleShowModal}>Detail</button></td>
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
@@ -160,14 +94,14 @@ export default class MasterUser extends Component {
 
                 <Modal show={this.state.isModalShow} onHide={this.handleCloseModal} size="xl" className="mt-5">
                     <Modal.Header closeButton>
-                        <Modal.Title>Detail</Modal.Title>
+                        <Modal.Title>{selectedUser.name}</Modal.Title>
                     </Modal.Header>
                     
                     <Modal.Body>
                         <div className="row">
                             <div className="col pt-4 pb-4">
-                                <h1 className="text-center text-success">83</h1>
-                                <h5 className="text-center font-weight-bold">Health Score Anda cukup baik</h5>
+                                <h1 className="text-center text-success">{selectedUser.healthScore}</h1>
+                                <h5 className="text-center font-weight-bold">Kesehatan Anda cukup baik</h5>
                                 <h6 className="text-center text-muted">Tingkatkan lagi kesehatan Anda dengan pola hidup yang baik</h6>
                             </div>
                         </div>
